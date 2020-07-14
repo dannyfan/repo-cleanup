02191bc0ed6b0b553bcf9ebb84ffa9a96e88dc30
Danny Fan <dannyfan@live.com>
2020-07-14 13:12:06 -0400

    fix: showing public and private repo in different columns

--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -4,28 +4,30 @@ import "./Repository.css";
 const Repository = (props) => {
     console.log(props);
 
-    const tag = props.data.private ? <span class="tag is-dark">Private</span> : <span class="tag is-info">Public</span>;
+    const tag = props.data.private ? <span className="tag is-dark">Private</span> : <span className="tag is-info">Public</span>;
     return (
-        <div className="Repository card" id={props.data.id}>
-            <header className="card-header">
-                <p className="card-header-title">
-                    {props.data.name}
-                </p>
-                {tag}
-            </header>
-            <div className="card-content">
-                <div className="content">
-                    <time dateTime={props.data.created_at}>Created: {props.data.created_at}</time>
-                    <br/>
-                    <time dateTime={props.data.updated_at}>Last update: {props.data.updated_at}</time>
-                    <br/>
-                    {props.data.description}
+        <li id={props.data.id}>
+            <div className="Repository card">
+                <header className="card-header">
+                    <p className="card-header-title">
+                        {props.data.name}
+                    </p>
+                    {tag}
+                </header>
+                <div className="card-content">
+                    <div className="content">
+                        <time dateTime={props.data.created_at}>Created: {props.data.created_at}</time>
+                        <br/>
+                        <time dateTime={props.data.updated_at}>Last update: {props.data.updated_at}</time>
+                        <br/>
+                        {props.data.description}
+                    </div>
                 </div>
+                <footer className="card-footer">
+                    <a className="button is-link card-footer-item" href={props.data.html_url}>Link</a>
+                </footer>
             </div>
-            <footer className="card-footer">
-                <a className="button is-link card-footer-item" href={props.data.html_url}>Link</a>
-            </footer>
-        </div>
+        </li>
     )
 }
 
--- src/components/Repository/RepositoryList.js
+++ src/components/Repository/RepositoryList.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import Repository from "./Repository";
 
 const RepositoryList = (props) => {
-    const [ repoList, setRepoList ] = useState({});
+    const [ publicList, setPublicList ] = useState({});
+    const [ privateList, setPrivateList ] = useState({});
 
     useEffect(() => {
         const getRepoList = () => {
-            const url = "https://api.github.com/user/repos?type=owner";
+            const url = `https://api.github.com/user/repos?type=owner`;
             fetch(url, {
                 method: 'GET',
                 headers: {
@@ -16,7 +17,10 @@ const RepositoryList = (props) => {
             })
             .then(response => response.json())
             .then(data => {
-                setRepoList(data);
+                const getPrivate = Object.values(data).filter(repo => repo.private);
+                const getPublic = Object.values(data).filter(repo => !repo.private);
+                setPublicList(getPublic);
+                setPrivateList(getPrivate);
             })
             .catch((err) => {
                 console.log(err);
@@ -27,11 +31,33 @@ const RepositoryList = (props) => {
     }, []);
     
     return (
-        <div className="Repository-list">{Object.values(repoList).map((val, key) => {
-            return (
-                <Repository key={ key } data={val}/>
-            )
-        })}</div>
+        <div className="Repository-container columns">
+            <div className="Repository-public column">
+                <h1 className="title">Column <span className="tag is-info">Public</span></h1>
+                <ul className="Repository-list">
+                    {Object.values(publicList).map((val, key) => {
+                        return (
+                            <Repository key={ key } data={val}/>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className="Repository-private column">
+                <h1 className="title">Column <span className="tag is-dark">Private</span></h1>
+                <ul className="Repository-list">
+                    {Object.values(privateList).map((val, key) => {
+                        return (
+                            <Repository key={ key } data={val}/>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className="Repository-delete column">
+                <h1 className="title">Column <span className="tag is-danger">Delete</span></h1>
+                <ul className="Repository-list">
+                </ul>
+            </div>
+        </div>
     )
 }
 
