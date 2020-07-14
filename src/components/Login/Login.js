967d53fe1cdbb0d24750ca1b74c6afcfc28c414b
Danny Fan <dannyfan@live.com>
2020-07-13 23:13:35 -0400

    fix: login authenication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react"
-import netlify from "netlify-auth-providers"
+import netlify from 'netlify-auth-providers'
 
 const Login = (props) => {
     const [ text, setText ] = useState("");
     const authenicateUser = () => {
-        const authenticator = new netlify.default ({});
-        authenticator.authenticate({provider:"github", scope: "user"}, function(err, data) {
+        const authenticator = new netlify({
+            site_id: process.env.REACT_APP_SITE_ID
+        });
+        const config = {
+            provider: "github",
+            scope: "repo, delete_repo, read:user"
+        };
+        authenticator.authenticate(config, function(err, data) {
             if (err) {
                 setText(err);
                 return
             }
-            setText(data.token);
+            props.setUser(data.token);
         });
-        props.setUser("danny");
-        console.log("Should login");
     }
+
     return (
         <>
             <button onClick={authenicateUser}>Login</button>
