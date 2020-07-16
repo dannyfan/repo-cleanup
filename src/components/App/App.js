7fd7811c37963eef0587d6c156f115dd2392655f
Danny Fan <dannyfan@live.com>
2020-07-16 00:19:35 -0400

    fix: spacing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Login from "../Login";
-import RepositoryList from '../Repository';
+import RepositoryList from "../Repository";
 import "bulma/css/bulma.css";
 
 const App = () => {
-  const [token, setToken] = useState("");
+    const [token, setToken] = useState("");
 
-  return (
-    <div className="App">
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Instructions</h1>
-          <p>Authorize github login with the "login" button to see all your repositories at once.
-            <br/>
-            <strong>Available actions: </strong> Make private repository to public, make public repository to private, delete repository.
-          </p>
-          <Login setToken={ setToken }/>
+    return (
+        <div className="App">
+            <section className="section">
+                <div className="container">
+                    <h1 className="title">Instructions</h1>
+                    <p>
+                        Authorize github login with the "login" button to see
+                        all your repositories at once.
+                        <br />
+                        <strong>Available actions: </strong> Make private
+                        repository to public, make public repository to private,
+                        delete repository.
+                    </p>
+                    <Login setToken={setToken} />
+                </div>
+            </section>
+            {token && <RepositoryList token={token} />}
         </div>
-      </section>
-      { token && <RepositoryList token={ token }/> }
-    </div>
-  )
-}
+    );
+};
 
 export default App;
